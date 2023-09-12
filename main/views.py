70e6f10df584d8ece7299ab6961a55cc3f9bdc58
aryakdaniswara <arya.kdaniswara@gmail.com>
2023-09-12 14:14:20 +0700

    update model and view

--- main/views.py
+++ main/views.py
@@ -3,9 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
+        'myName' : 'Arya Kusuma Daniswara',
+        'class' : 'PBP B',
+        'appName' : 'inventory00',
         'name': 'Botol',
         'amount': '2',
-        'description' : 'Botol 500ml yang sangat bagus'
+        'description' : 'Botol 500 ml yang sangat bagus'
     }
 
     return render(request, "main.html", context)
