2f203870292ee8fbee429a2a7372564119d65fd4
aryakdaniswara <arya.kdaniswara@gmail.com>
2023-09-19 23:08:48 +0700

    Adding form and item count, fix test

--- main/models.py
+++ main/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Item(models.Model):
     name = models.CharField(max_length=255)
-    amount = models.IntegerField()
-    description = models.TextField()
+    amount = models.IntegerField(default=0)
+    description = models.TextField(default="")
     price = models.IntegerField(default=0)
+    date_added = models.DateField(auto_now_add=True)
--- main/tests.py
+++ main/tests.py
@@ -2,21 +2,13 @@ from django.test import TestCase
 
 # Create your tests here.
 from django.test import TestCase, Client
-from .models import Item
+from main.models import Item
 
 class mainTest(TestCase):
-    def test_main_url_is_exist(self):
-        response = Client().get('/main/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_main_using_main_template(self):
-        response = Client().get('/main/')
-        self.assertTemplateUsed(response, 'main.html')
-    
     def test_name_field(self):
-        item = Item.objects.create(name='name')
+        item = Item.objects.create(name='name', amount=1, description='tes', price=1)
         self.assertEqual(item.name, 'name')
 
     def test_name_max_length(self):
-        item = Item.objects.create(name='KLqa8pXqtliI9wwUDG7ruYIFWjRDJZFsySpuabY12HEgWWnbvdywu2Zl8yGUiVZIbwhHEIiuCPfc8EnlRf6Okzbb3ocihi9pRaUmS3QpHeQoGctIV842FF2WUUKLIQ7LzVFQ1cdwrmz1lbuSg1DU0ActIsMdcSsWiyUOfOk1SsvYlFpZiP4UkGpWe3sHyiZxCzDZJLVVJoZIRhmYOTH1v4sHjoVNvFWqAhREiau4HQB2uC9faDZE17bRxI6Cc2h')
+        item = Item.objects.create(name='KLqa8pXqtliI9wwUDG7ruYIFWjRDJZFsySpuabY12HEgWWnbvdywu2Zl8yGUiVZIbwhHEIiuCPfc8EnlRf6Okzbb3ocihi9pRaUmS3QpHeQoGctIV842FF2WUUKLIQ7LzVFQ1cdwrmz1lbuSg1DU0ActIsMdcSsWiyUOfOk1SsvYlFpZiP4UkGpWe3sHyiZxCzDZJLVVJoZIRhmYOTH1v4sHjoVNvFWqAhREiau4HQB2uC9faDZE17bRxI6Cc2h', amount=1, description='tes', price=1)
         self.assertEqual(item.name, 'KLqa8pXqtliI9wwUDG7ruYIFWjRDJZFsySpuabY12HEgWWnbvdywu2Zl8yGUiVZIbwhHEIiuCPfc8EnlRf6Okzbb3ocihi9pRaUmS3QpHeQoGctIV842FF2WUUKLIQ7LzVFQ1cdwrmz1lbuSg1DU0ActIsMdcSsWiyUOfOk1SsvYlFpZiP4UkGpWe3sHyiZxCzDZJLVVJoZIRhmYOTH1v4sHjoVNvFWqAhREiau4HQB2uC9faDZE17bRxI6Cc2h')
